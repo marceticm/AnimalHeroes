aa47684b37402dfbe5b670c1a69447e4a25ec4e8
Milan Marcetic <milan.marcetic@startelecom.ca>
2021-03-23 16:56:12 +0100

    Started with client-side login implementation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,19 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent
-  ],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn: boolean;
 
-  constructor() { }
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  logout() {
+    this.loggedIn = false;
+  }
 }
