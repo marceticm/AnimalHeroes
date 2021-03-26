38f138bb867644baeb4cbd6bd6dca535c693c646
Milan Marcetic <milan.marcetic@startelecom.ca>
2021-03-26 10:33:35 +0100

    - Created a Test Error API controller - Added string length validation for password

---
+++ API/Controllers/ErrorTestController.cs
@@ -0,0 +1,46 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ErrorTestController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ErrorTestController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "some secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var user = _context.Users.Find(-1);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var user = _context.Users.Find(-1);
+            var ret = user.ToString();
+            return ret;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("test bad request");
+        }
+    }
+}
