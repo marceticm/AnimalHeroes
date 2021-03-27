ec8cde59d91666cbf158cc7f84d3f6d7641b9a97
DESKTOP-TV2BBR1\Milan <marcetic18@hotmail.com>
2021-03-27 16:56:26 +0100

    - Extended the User entity - Added Photo entity - Seeding data - Repository pattern - Automapper

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,7 @@
-﻿namespace API.Entities
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Entities
 {
     public class AppUser
     {
@@ -6,5 +9,20 @@
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        // don't need this cause it's called in AutoMapperProfiles
+        //public int GetAge() 
+        //{
+        //    return DateOfBirth.CalculateAge();
+        //}
     }
 }
